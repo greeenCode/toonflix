9c285a19ef90cb1c4d3d5fc40dcad6a4c6e492d6
greeenCode <ie-woo@hanmail.net>
2023-08-28 21:56:34 +0900

    #6.5 waitForWebToons-

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,13 +4,14 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
