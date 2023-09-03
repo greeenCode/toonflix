5cf8f964b75eee60ca4039f02b87987bff2c0032
greeenCode <ie-woo@hanmail.net>
2023-09-03 17:04:41 +0900

    #6.10 Hero

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -16,54 +16,62 @@ class Webtoon extends StatelessWidget {
     return GestureDetector(
       onTap: () {
         Navigator.push(
-            context,
-            // MaterialPageRoute(
-            //   builder: (context) => DetailScreen(
-            //     title: title,
-            //     thumb: thumb,
-            //     id: id,
-            //   ),
-            //   fullscreenDialog: true,
-            // ),
-            PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) =>
-                  DetailScreen(
-                title: title,
-                thumb: thumb,
-                id: id,
-              ),
-              transitionsBuilder:
-                  (context, animation, secondaryAnimation, child) {
-                return SlideTransition(
-                  position: Tween<Offset>(
-                    begin: const Offset(1.0, 0.0),
-                    end: Offset.zero,
-                  ).animate(animation),
-                  child: child,
-                );
-              },
-            ));
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
+            fullscreenDialog: true,
+          ),
+        );
+        // PageRouteBuilder(
+        //   pageBuilder: (context, animation, secondaryAnimation) =>
+        //       DetailScreen(
+        //     title: title,
+        //     thumb: thumb,
+        //     id: id,
+        //   ),
+        //   transitionsBuilder:
+        //       (context, animation, secondaryAnimation, child) {
+        //     final curvedAnimation = CurvedAnimation(
+        //       parent: animation,
+        //       curve: Curves.easeInOut, // 여기에 원하는 곡선을 선택
+        //     );
+        //     return SlideTransition(
+        //       position: Tween<Offset>(
+        //         begin: const Offset(1.0, 0.0),
+        //         end: Offset.zero,
+        //       ).animate(curvedAnimation),
+        //       child: child,
+        //     );
+        //   },
+        // ));
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.3),
-                  )
-                ]),
-            child: Image.network(
-              thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                    )
+                  ]),
+              child: Image.network(
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
           const SizedBox(
