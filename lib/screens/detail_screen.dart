724a1c2102d544c3cb184c378abb64c92c9d1a48
greeenCode <ie-woo@hanmail.net>
2023-09-06 16:45:27 +0900

    #6.14 Detail Info

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episodes_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class DetailScreen extends StatelessWidget {
   final String title, thumb, id;
 
-  const DetailScreen({
+  DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
+
+  late final Future<WebtoonDetailModel> webtoon = ApiService.getToonById(id);
+  late final Future<List<WebtoonEpisodesModel>> episodes =
+      ApiService.getLatestEpisodesById(id);
 
   @override
   Widget build(BuildContext context) {
@@ -59,6 +66,45 @@ class DetailScreen extends StatelessWidget {
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error : ${snapshot.error}');
+              } else if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              return const Text("....");
+            },
+          )
         ],
       ),
     );
