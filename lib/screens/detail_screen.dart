631192c12b697ccfb4827fd7cddcf18d5ef12cdb
greeenCode <ie-woo@hanmail.net>
2023-09-10 16:25:33 +0900

    #6.17 Favorites

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episodes_model.dart';
 import 'package:toonflix/services/api_service.dart';
 import 'package:toonflix/widgets/episode_widget.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
-  DetailScreen({
+  const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
 
-  late final Future<WebtoonDetailModel> webtoon = ApiService.getToonById(id);
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late final Future<WebtoonDetailModel> webtoon =
+      ApiService.getToonById(widget.id);
   late final Future<List<WebtoonEpisodesModel>> episodes =
-      ApiService.getLatestEpisodesById(id);
+      ApiService.getLatestEpisodesById(widget.id);
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPrefs();
+  }
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      prefs.setStringList('likedToons', []);
+    }
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,12 +71,20 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         centerTitle: true,
-        title: const Text(
-          '오늘의 웹툰',
-          style: TextStyle(
-            fontSize: 24,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 20,
           ),
         ),
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: isLiked
+                ? const Icon(Icons.favorite)
+                : const Icon(Icons.favorite_outline),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -46,7 +98,7 @@ class DetailScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Hero(
-                    tag: id,
+                    tag: widget.id,
                     child: Container(
                       width: 250,
                       clipBehavior: Clip.hardEdge,
@@ -60,7 +112,7 @@ class DetailScreen extends StatelessWidget {
                             )
                           ]),
                       child: Image.network(
-                        thumb,
+                        widget.thumb,
                         headers: const {
                           "User-Agent":
                               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
@@ -114,7 +166,7 @@ class DetailScreen extends StatelessWidget {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Episode(episode: episode, webtoonId: id)
+                          Episode(episode: episode, webtoonId: widget.id)
                       ],
                     );
                   }
